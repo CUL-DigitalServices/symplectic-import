b8ced648d73c0d0de47a61d8389de513885b248d
Mathieu Decoene <hello@mathieudecoene.be>
2014-09-08 16:41:05 +0100

    Added file export

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-Symplectic import
+Symplectic import tool
 =====
+
+Install
+=======
+
+`npm install -d`
+
+Run the script
+===========================
+
+```
+~$ ./bin/import.js --help
+Usage: ./bin/import.js --target-file [--groups] [--created-since] [--ever-approved]
+
+Options:
+  -t, --target-file     The directory where the file will be exported to (e.g. ~/Documents)
+  -g, --groups          The group where the publications belong to
+  -c, --created-since   The insertion start date of the publications (e.g. 2014-09-01')
+  -e, --ever-approved   Whether the publications need to be approved or not (default: true)
+```
--- bin/import.js
+++ bin/import.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 
 var _ = require('underscore');
+var colors = require('colors');
+var fs = require('fs');
 var q = require('q');
-var request = require('request');
 var util = require('util');
-var xml2js = require('xml2js');
 var yargs = require('yargs');
 
+var API = require('../lib/api');
 var Constants = require('../lib/constants').Constants;
-var SymplecticPublication = require('../lib/model').SymplecticPublication;
 
 var argv = yargs
-    .usage('Usage: ./bin/import.js [--groups] [--created-since] [--ever-approved]')
+    .usage('Usage: ./bin/import.js --target-file [--groups] [--created-since] [--ever-approved]')
+
+    .alias('-t')
+    .describe('-t', 'The directory where the file will be exported to (e.g. ~/Documents)')
 
     .alias('-g', 'groups')
     .describe('-g', 'The group where the publications belong to')
@@ -19,10 +22,61 @@ var argv = yargs
     .alias('-c', 'created-since')
     .describe('-c', 'The insertion start date of the publications (e.g. 2014-09-01')
 
-    .alias('-e')
-    .describe('-e', 'Whether the publications need to be approved or not (default: true).')
-
+    .alias('-e', 'ever-approved')
+    .describe('-e', 'Whether the publications need to be approved or not (default: true)')
     .argv;
+
+/**
+ * Initialize the script
+ */
+var init = function() {
+
+    // Check if the parameter has been set
+    if (!argv.t) {
+        console.log('Location for exported file missing'.red);
+        process.exit(0);
+    }
+
+    // Check if the specified location exists
+    if (!fs.existsSync(argv.t)) {
+        console.log('Invalid export location'.red);
+        process.exit(0);
+    }
+
+    // Perform the API request
+    API.doRequest(constructQueryString())
+
+        // Export the results
+        .then(exportResults)
+
+        // Errorhandler
+        .catch(function(err) {
+            console.log(err);
+        });
+};
+
+/**
+ * Exports the results to a JSON-file
+ *
+ * @param {Publication[]}   publications    A collection of publications
+ * @return {Promise}
+ * @api private
+ */
+var exportResults = function(publications) {
+    var deferred = q.defer();
+
+    var fileName = util.format('%s/publications.json', argv.t);
+    fs.writeFile(fileName, JSON.stringify(publications, null, 4), 'utf8', function(err) {
+        if (err) {
+            return deferred.reject('Error while exporting publications');
+        }
+
+        console.log(util.format('publications exported at %s', fileName).green);
+        deferred.resolve();
+    });
+
+    return deferred.promise;
+};
 
 /**
  * Constructs a query string based on the command line parameters
@@ -30,6 +84,9 @@ var argv = yargs
  * @return {String}     The created query string
  */
 var constructQueryString = function() {
+    var errors = [];
+
+    // Caches the query string parameters
     var queryString = ['detail=full'];
 
     if (argv.g) {
@@ -38,7 +95,7 @@ var constructQueryString = function() {
 
     if (argv.c) {
         if (argv.c.length !== 10) {
-            return console.log('Invalid value for \'created-since\'');
+            errors.push('Invalid value for \'created-since\'');
         }
         queryString.push(util.format('created-since=%sT00%3A00%3A00%2B01%3A00', argv.c))
     }
@@ -46,79 +103,20 @@ var constructQueryString = function() {
     var everApproved = true;
     if (argv.e && argv.e !== 'true') {
         if (argv.e !== 'false') {
-            return console.log('Invalid value for \'ever-approved\'');
+            errors.push('Invalid value for \'ever-approved\'');
         }
         everApproved = false;
     }
     queryString.push(util.format('ever-approved=%s', everApproved));
+
+    // Stop the progress if any errors occurred
+    if (errors.length) {
+        console.log(errors[0].red);
+        process.exit(1);
+    }
+
+    // Return the created query string
     return util.format('%s?%s', Constants.API.URI, queryString.sort().join('&'));
 };
 
-/**
- * Perform a request to the Symplectic API
- *
- * @api private
- */
-var doAPIRequest = function() {
-    var url = constructQueryString();
-
-    // Send a request to the Symplectic API
-    request({'url': url}, function(err, response) {
-        if (err) {
-            console.log(err);
-        }
-
-        // Parse the response body
-        parseXml(response.body)
-
-            // Parse the results
-            .then(parseResults)
-
-            // Errorhandler
-            .catch(function(err) {
-                console.log(err);
-            });
-    });
-};
-
-/**
- * Takes a string and parses it as XML
- *
- * @param  {String}     str     The string to parse
- * @api private
- */
-var parseXml = function(str) {
-    var deferred = q.defer();
-
-    try {
-        var parser = new xml2js.Parser({'explicitArray': true});
-        parser.parseString(str, function(err, xml) {
-            if (err) {
-                return deferred.reject({'code': 500, 'msg': 'Could not parse XML'})
-            }
-            return deferred.resolve(xml);
-        });
-    } catch (error) {
-        return deferred.reject({'code': 500, 'msg': 'Could not parse XML'})
-    }
-
-    return deferred.promise;
-};
-
-/**
- * Parses the publications
- *
- * @param  {String}     xml                 Object containing the XML data
- * @api private
- */
-var parseResults = function(xml) {
-
-    // Loop the entries
-    var entries = _.map(xml.feed.entry, function(entry) {
-        return new SymplecticPublication(entry);
-    });
-
-    return entries;
-};
-
-doAPIRequest();
+init();
